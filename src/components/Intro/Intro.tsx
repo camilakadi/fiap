3f1f65b9911d70b7c447edb10ee7ed39d6f8c3a2
Camila Kadi <camilakadi@hotmail.com>
2025-09-28 16:36:04 -0300

    feat: implementa animação da imagem de intro e marquee abaixo dela

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { gsap } from "gsap";
-import Image from "next/image";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect, useRef } from "react";
 import styles from "./Intro.module.scss";
 
 export default function Intro() {
   const marquee01Ref = useRef<HTMLDivElement>(null);
   const marquee02Ref = useRef<HTMLDivElement>(null);
+  const marquee03Ref = useRef<HTMLDivElement>(null);
+  const marquee04Ref = useRef<HTMLDivElement>(null);
+  const introBannerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
     if (marquee01Ref.current) {
       gsap.set(marquee01Ref.current, { x: "0%" });
       gsap.to(marquee01Ref.current, {
@@ -29,6 +34,49 @@ export default function Intro() {
         repeat: -1,
       });
     }
+
+    if (introBannerRef.current) {
+      gsap.set(introBannerRef.current, { height: 0 });
+      gsap.to(introBannerRef.current, {
+        height: "804px",
+        scrollTrigger: {
+          trigger: "#marquee03",
+          start: "top bottom",
+          scrub: false,
+        },
+        duration: 1,
+        ease: "power2.out",
+      });
+    }
+
+    if (marquee03Ref.current) {
+      gsap.set(marquee03Ref.current, { x: "0%" });
+
+      gsap.to(marquee03Ref.current, {
+        x: "-75%",
+        scrollTrigger: {
+          trigger: "#marquee03",
+          start: "top-=100 bottom",
+          end: "bottom+=100 top",
+          scrub: true,
+        },
+        ease: "power1.out",
+      });
+    }
+
+    if (marquee04Ref.current) {
+      gsap.set(marquee04Ref.current, { x: "-75%" });
+      gsap.to(marquee04Ref.current, {
+        x: "0%",
+        scrollTrigger: {
+          trigger: "#marquee04",
+          start: "top-=100 bottom",
+          end: "bottom+=100 top",
+          scrub: true,
+        },
+        ease: "power1.out",
+      });
+    }
   }, []);
 
   return (
@@ -39,12 +87,15 @@ export default function Intro() {
             <span className={styles.marqueeContent}>
               CURSOS E IMERSÕES. &nbsp;
             </span>
+
             <span className={styles.marqueeContent}>
               UMA NOVA CULTURA DE MERCADO. &nbsp;
             </span>
+
             <span className={styles.marqueeContent}>
               CURSOS E IMERSÕES. &nbsp;
             </span>
+
             <span className={styles.marqueeContent}>
               UMA NOVA CULTURA DE MERCADO. &nbsp;
             </span>
@@ -56,12 +107,15 @@ export default function Intro() {
             <span className={styles.marqueeContent}>
               TECNOLOGIA, INOVAÇÃO E NEGÓCIOS. &nbsp;
             </span>
+
             <span className={styles.marqueeContent}>
               PRESENTE E FUTURO. &nbsp;
             </span>
+
             <span className={styles.marqueeContent}>
               TECNOLOGIA, INOVAÇÃO E NEGÓCIOS. &nbsp;
             </span>
+
             <span className={styles.marqueeContent}>
               PRESENTE E FUTURO. &nbsp;
             </span>
@@ -69,64 +123,61 @@ export default function Intro() {
         </div>
       </div>
 
-      <div className={styles.banner}>
-        <Image
-          src="/imgs/intro.png"
-          alt="Menino escutando música e pensando"
-          width={1495}
-          height={804}
-          priority
-          className={styles.imageBanner}
-        />
+      <div className={styles.introBanner}>
+        <div className={styles.introBannerContent} ref={introBannerRef}>
+          <div className={styles.introBannerImageContainer}>
+            <picture>
+              <img
+                src="/imgs/intro.png"
+                alt="Menino escutando música e pensando"
+                width={1495}
+                className={styles.introBannerImage}
+              />
+            </picture>
+          </div>
+        </div>
       </div>
 
-      <div className={styles.carouselTextoCima}>
-        <Image
-          src="/imgs/skills.png"
-          alt="Skills"
-          width={500}
-          height={121}
-          priority
-          className={styles.carouselTextoCimaSkills}
-        />
+      <div className={styles.marquees02}>
+        <div className={styles.marqueeContainer02}>
+          <div
+            id="marquee03"
+            className={styles.marqueeTrack02}
+            ref={marquee03Ref}
+          >
+            <div className={styles.marqueeContent02}>Skills</div>
 
-        <Image
-          src="/imgs/circulo.png"
-          alt="Círculo"
-          width={38}
-          height={38}
-          priority
-          className={styles.carouselTextoCimaCirculo}
-        />
+            <div className={styles.circle}></div>
 
-        <Image
-          src="/imgs/conhecimento.png"
-          alt="Conhecimento"
-          width={1261}
-          height={121}
-          priority
-          className={styles.carouselTextoCimaConhecimento}
-        />
-      </div>
+            <div className={styles.marqueeContent02}>Conhecimento</div>
 
-      <div className={styles.carouselTextoBaixo}>
-        <Image
-          src="/imgs/muito-alem-dos-tutoriais.png"
-          alt="Muito além dos tutoriais"
-          width={1940}
-          height={121}
-          priority
-          className={styles.carouselTextoBaixoSkills}
-        />
+            <div className={styles.circle}></div>
 
-        <Image
-          src="/imgs/circulo.png"
-          alt="Círculo"
-          width={38}
-          height={38}
-          priority
-          className={styles.carouselTextoBaixoCirculo}
-        />
+            <div className={styles.marqueeContent02}>Skills</div>
+
+            <div className={styles.circle}></div>
+
+            <div className={styles.marqueeContent02}>Conhecimento</div>
+          </div>
+        </div>
+
+        <div className={styles.marqueeContainer02}>
+          <div
+            id="marquee04"
+            className={styles.marqueeTrack02}
+            ref={marquee04Ref}
+          >
+            <div className={styles.marqueeContent02}>
+              Muito além dos tutoriais
+            </div>
+
+            <div className={styles.circle}></div>
+
+            <div className={styles.marqueeContent02}>
+              Muito além dos tutoriais
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
