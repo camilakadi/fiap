9f3cae6c7643aad15e02a1a8fc3148204b2d0263
Camila Kadi <camilakadi@hotmail.com>
2025-09-28 02:44:55 -0300

    feat: implementa a marquee na intro

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,12 +1,72 @@
+"use client";
+
+import { gsap } from "gsap";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 import styles from "./Intro.module.scss";
 
 export default function Intro() {
+  const marquee01Ref = useRef<HTMLDivElement>(null);
+  const marquee02Ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (marquee01Ref.current) {
+      gsap.set(marquee01Ref.current, { x: "0%" });
+      gsap.to(marquee01Ref.current, {
+        x: "-50%",
+        duration: 10,
+        ease: "none",
+        repeat: -1,
+      });
+    }
+
+    if (marquee02Ref.current) {
+      gsap.set(marquee02Ref.current, { x: "-50%" });
+      gsap.to(marquee02Ref.current, {
+        x: "0%",
+        duration: 10,
+        ease: "none",
+        repeat: -1,
+      });
+    }
+  }, []);
+
   return (
     <section className={styles.intro}>
-      <div className={styles.introText}>
-        <div>CURSOS E IMERSÕES. UMA NOVA CULTURA DE MERCADO.</div>
-        <div>TECNOLOGIA, INOVAÇÃO E NEGÓCIOS. PRESENTE E FUTURO.</div>
+      <div className={styles.marquees}>
+        <div className={styles.marqueeContainer}>
+          <div className={styles.marqueeTrack} ref={marquee01Ref}>
+            <span className={styles.marqueeContent}>
+              CURSOS E IMERSÕES. &nbsp;
+            </span>
+            <span className={styles.marqueeContent}>
+              UMA NOVA CULTURA DE MERCADO. &nbsp;
+            </span>
+            <span className={styles.marqueeContent}>
+              CURSOS E IMERSÕES. &nbsp;
+            </span>
+            <span className={styles.marqueeContent}>
+              UMA NOVA CULTURA DE MERCADO. &nbsp;
+            </span>
+          </div>
+        </div>
+
+        <div className={styles.marqueeContainer}>
+          <div className={styles.marqueeTrack} ref={marquee02Ref}>
+            <span className={styles.marqueeContent}>
+              TECNOLOGIA, INOVAÇÃO E NEGÓCIOS. &nbsp;
+            </span>
+            <span className={styles.marqueeContent}>
+              PRESENTE E FUTURO. &nbsp;
+            </span>
+            <span className={styles.marqueeContent}>
+              TECNOLOGIA, INOVAÇÃO E NEGÓCIOS. &nbsp;
+            </span>
+            <span className={styles.marqueeContent}>
+              PRESENTE E FUTURO. &nbsp;
+            </span>
+          </div>
+        </div>
       </div>
 
       <div className={styles.banner}>
