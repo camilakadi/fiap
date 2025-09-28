358f1050182ece023cb1e8eb8cddb103e215c191
Camila Kadi <camilakadi@hotmail.com>
2025-09-28 17:41:46 -0300

    feat: implementa barra de progresso no header e fixa header ao scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,29 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "./Header.module.scss";
 
 export default function Header() {
+  const [scrollProgress, setScrollProgress] = useState(0);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const updateScrollProgress = () => {
+      const scrollTop = window.scrollY;
+      const docHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const progress = (scrollTop / docHeight) * 100;
+      setScrollProgress(progress);
+      setIsScrolled(scrollTop > 0);
+    };
+
+    window.addEventListener("scroll", updateScrollProgress);
+    return () => window.removeEventListener("scroll", updateScrollProgress);
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isScrolled ? styles.scrolled : ""}`}>
       <div className={styles.title}>
         <Image
           src="./svgs/logo-fiap.svg"
@@ -13,6 +33,10 @@ export default function Header() {
           priority
         />
       </div>
+      <div
+        className={styles.progressBar}
+        style={{ width: `${scrollProgress}%` }}
+      />
     </header>
   );
 }
